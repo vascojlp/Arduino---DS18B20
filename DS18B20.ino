eb1a29733ee934c56f61189469202df89f641edb
vascojlp <vascojlp@gmail.com>
2016-03-10 21:25:46 +0000

    Added files via upload

---
+++ DS18B20.ino
@@ -0,0 +1,26 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <LiquidCrystal.h>
+
+#define ONE_WIRE_BUS 10
+LiquidCrystal lcd(13,12,11,10,9,8);
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+void setup() {
+  lcd.begin(20, 4);
+  sensors.begin();
+}
+
+void loop() {
+  sensors.requestTemperatures();
+  lcd.setCursor(0, 0);
+  lcd.print("Sensor 0: ");
+  lcd.print(sensors.getTempCByIndex(0));
+  lcd.print("C");
+  
+  lcd.setCursor(0, 1);
+  lcd.print("Sensor 1: ");
+  lcd.print(sensors.getTempCByIndex(1));
+  lcd.print("C");
+}
